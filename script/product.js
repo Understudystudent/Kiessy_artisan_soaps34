41c2e4c670976419b7fd61a0a72e07a436a6626b
Carl James <carlmjames2@gmail.com>
2023-12-03 16:42:49 -0500

    added heading and started with javascript

--- script/product.js
+++ script/product.js
@@ -1,1 +1,13 @@
-//code
+document.querySelector('#currentYear').textContent = new Date().getFullYear();
+
+let products = JSON.parse(localStorage.getItem('products')) ? JSON.parse(localStorage.getItem('products')) :
+localStorage.setItem('products', JSON.stringify(
+    [
+        {
+            "id":1,
+            "make": Lavendear,
+            "amount":R45,
+            "img":https://i.postimg.cc/28QgBc5r/ed929619-a18f-484e-9543-6b401a5df5c5.jpg
+        }
+    ]
+))
